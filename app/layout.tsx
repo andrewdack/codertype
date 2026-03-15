c4eeb0a4a4846a88651d58facaa0f1aec2ea4019
Andrew Hu <andrewhu32009@gmail.com>
2026-03-15 17:48:29 -0400

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Roboto_Mono } from "next/font/google";
+import { Geist, Roboto_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -24,7 +27,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={cn("font-mono", jetbrainsMono.variable)}>
             <body
                 className={`${geistSans.variable} ${robotoMono.variable} font-mono antialiased`}
             >
