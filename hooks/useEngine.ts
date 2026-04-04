f89554b79e3fdae0be7e6527265c339213992dc2
Andrew Hu <andrewhu32009@gmail.com>
2026-04-03 21:49:35 -0400

    feat: wpm time tracking each second in preparation for the stat graph

--- hooks/useEngine.ts
+++ hooks/useEngine.ts
@@ -4,12 +4,23 @@ import useTimer, { TimerMode } from "./useTimer";
 import useTypings from "./useTypings";
 import { type Language, type Length } from "@/lib/snippets";
 import { generateBracketPairs } from "@/utils/bracketMatching";
+import {
+    calculateAccuracyPercentage,
+    calculateAdjustedWPM,
+    calculateRawWPM,
+} from "@/utils/stats";
 
 export type State = "start" | "run" | "finish";
 export type { TimerMode };
 
+type WPMpoint = {
+    timeMs: number;
+    rawWpm: number;
+    adjWpm: number;
+    errors: number;
+    totalTyped: number;
+};
 
-    
 export default function useEngine(
     language: Language = "javascript",
     timerMode: TimerMode,
@@ -18,7 +29,11 @@ export default function useEngine(
 ) {
     const [state, setState] = useState<State>("start");
     const { snippet, nextSnippet } = useSnippet(language, lengthFilter);
-    const { value: timerValue, start: startTimer, reset: resetTimer } = useTimer(timerMode, countdownSeconds);
+    const {
+        value: timerValue,
+        start: startTimer,
+        reset: resetTimer,
+    } = useTimer(timerMode, countdownSeconds);
 
     const [autoCompleteBrackets, setAutoCompleteBrackets] = useState(true);
 
@@ -26,19 +41,29 @@ export default function useEngine(
     const [startTime, setStartTime] = useState(-1);
     const [endTime, setEndTime] = useState(-1);
 
+    const [wpmHistory, setWpmHistory] = useState<WPMpoint[]>([]);
+
     const bracketPairs = useMemo(() => generateBracketPairs(words), [words]);
 
-    const [correctlyTypedOpenings, setCorrectlyTypedOpenings] = useState<Set<number>>(new Set());
+    const [correctlyTypedOpenings, setCorrectlyTypedOpenings] = useState<
+        Set<number>
+    >(new Set());
 
-    const { typed, cursor, clearTyped, resetTotalTyped, totalTyped, totalErrors } =
-        useTypings(
-            state !== "finish",
-            words,
-            bracketPairs,
-            correctlyTypedOpenings,
-            setCorrectlyTypedOpenings,
-            autoCompleteBrackets,
-        );
+    const {
+        typed,
+        cursor,
+        clearTyped,
+        resetTotalTyped,
+        totalTyped,
+        totalErrors,
+    } = useTypings(
+        state !== "finish",
+        words,
+        bracketPairs,
+        correctlyTypedOpenings,
+        setCorrectlyTypedOpenings,
+        autoCompleteBrackets,
+    );
 
     const isStarting = state === "start" && cursor > 0;
     const areWordsFinished = typed.length === words.length;
@@ -75,6 +100,41 @@ export default function useEngine(
             setCorrectlyTypedOpenings(new Set());
         }
     }, [autoCompleteBrackets]);
+
+    // wpm tracking over time
+    useEffect(() => {
+        if (state !== "run") {
+            return;
+        }
+
+        const interval = setInterval(() => {
+            const now = Date.now();
+            const elapsed = now - startTime;
+
+            if (elapsed <= 0) return;
+
+            const rawWpm = calculateRawWPM(totalTyped, elapsed);
+            const adjWpm = calculateAdjustedWPM(
+                rawWpm,
+                calculateAccuracyPercentage(totalErrors, totalTyped),
+            );
+            setWpmHistory((prev) => {
+                const newHistory = [
+                    ...prev,
+                    {
+                        timeMs: elapsed,
+                        rawWpm,
+                        adjWpm,
+                        errors: totalErrors,
+                        totalTyped: totalTyped,
+                    },
+                ];
+                console.log(newHistory);
+                return newHistory;
+            });
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [state, startTime, totalTyped, totalErrors]);
 
     const restart = useCallback(() => {
         resetTimer();
