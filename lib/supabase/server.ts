30c09edcd452c2916027313cea27e21aee89e27d
Andrew Hu <andrewhu32009@gmail.com>
2026-04-10 03:10:41 -0400

    supabase client, server, and middleware

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,34 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createClient() {
-    const cookieStore = cookies();
+export async function createClient() {
+    const cookieStore = await cookies(); // give access to request cookies
+    // how to read write auth cookies
+    return createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                // read cookies from request
+                getAll() { return cookieStore.getAll(); },
+                // writes cookies to the response
+                setAll(cookiesToSet) {
+                    try {
+                        cookiesToSet.forEach(({ name, value, options }) => {
+                            cookieStore.set({
+                                name,
+                                value,
+                                ...options,
+                            });
+                        });
+                    }
+                    catch {
+
+                    }
+                }
+            }
+        }
+    );
+
 
 }
